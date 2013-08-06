6bd5c8b606405f5e1e4725c5cbd2b502be717507
Olav Andreas Lindekleiv <olav@lindekleiv.com>
2013-08-06 20:08:26 +0200

    Added test with nested JSON object.

--- README.md
+++ README.md
@@ -3,4 +3,38 @@ Schema
 
 A small library for validating JSON.
 
-More tests, examples and documentation will be added soon.
+## Simple example
+```Go
+data := []byte(`{
+	"Title": "About me",
+	"Websites": {
+		"Twitter": {
+			"Norwegian": "@oav",
+			"English": "@lindekleiv"
+		},
+		"Github": "oal",
+		"BitBucket": "lindekleiv"
+	}
+}`)
+
+var v map[string]interface{}
+err := json.Unmarshal(data, &v)
+
+schema := map[string]interface{}{
+	"Title": Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{25}},
+	"Websites": map[string]interface{}{
+		"Twitter": map[string]interface{}{
+			"Norwegian": Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{20}},
+			"English":   Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{20}},
+		},
+		"Tumblr":    Validator{required: false, fun: Builtin.MaxLength, args: []interface{}{100}},
+		"Github":    Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{39}},
+		"BitBucket": Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{39}},
+	},
+}
+
+err = Validate(v, schema)
+if err != nil {
+	panic(err)
+}
+```
--- schema_test.go
+++ schema_test.go
@@ -7,16 +7,51 @@ import (
 
 func TestBasic(t *testing.T) {
 	data := []byte(`{
-		"title": "A basic test",
-		"intValue": 50
-    }`)
+		"Title": "A basic test",
+		"IntValue": 50
+	}`)
 
 	var v map[string]interface{}
 	err := json.Unmarshal(data, &v)
 
 	schema := map[string]interface{}{
-		"title":    Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{25}},
-		"intValue": Validator{required: true, fun: Builtin.Integer},
+		"Title":    Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{25}},
+		"IntValue": Validator{required: true, fun: Builtin.Integer},
+	}
+
+	err = Validate(v, schema)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestBasicRecursive(t *testing.T) {
+	data := []byte(`{
+		"Title": "About me",
+		"Websites": {
+			"Twitter": {
+				"Norwegian": "@oav",
+				"English": "@lindekleiv"
+			},
+			"Github": "oal",
+			"BitBucket": "lindekleiv"
+		}
+	}`)
+
+	var v map[string]interface{}
+	err := json.Unmarshal(data, &v)
+
+	schema := map[string]interface{}{
+		"Title": Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{25}},
+		"Websites": map[string]interface{}{
+			"Twitter": map[string]interface{}{
+				"Norwegian": Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{20}},
+				"English":   Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{20}},
+			},
+			"Tumblr":    Validator{required: false, fun: Builtin.MaxLength, args: []interface{}{100}},
+			"Github":    Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{39}},
+			"BitBucket": Validator{required: true, fun: Builtin.MaxLength, args: []interface{}{39}},
+		},
 	}
 
 	err = Validate(v, schema)
